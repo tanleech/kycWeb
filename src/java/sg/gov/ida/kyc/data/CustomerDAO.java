3f28c162e8b82ac1a4503b1e86768f21af3e8e3f
ftbs <ftbs@FTBS_HQ>
2016-04-26 17:56:12 +0800

    refactor

---
+++ src/java/sg/gov/ida/kyc/data/CustomerDAO.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sg.gov.ida.kyc.data;
+
+
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.LogicalExpression;
+import org.hibernate.criterion.Restrictions;
+
+/**
+ *
+ * @author Mike
+ */
+public class CustomerDAO {
+    
+    public void createCustomer(CustomerDto custDto){
+        Session session = null;
+        Transaction txn = null;
+        try
+        {
+            session =  DaoDelegate.getInstance().create();
+            txn = session.beginTransaction();
+            
+            session.save(custDto);
+            txn.commit();
+            
+        }catch (Exception ex)
+        {
+            if(txn!=null)
+            {
+                txn.rollback();
+            }
+            ex.printStackTrace();
+        }
+        finally
+        {
+            DaoDelegate.getInstance().close(session);
+        }
+    }
+    public List<CustomerDto> search(String name, String uid)
+    {
+        Session session = null;
+        List custList = null;
+        try
+        {
+            session =  DaoDelegate.getInstance().create();
+            Criteria cr = session.createCriteria(CustomerDto.class);
+            // Add restriction.
+            Criterion nameR=null,uidR=null;
+            if(name!=null&& !name.isEmpty())
+               nameR = Restrictions.like("name", "%"+name+"%");
+            if(uid!=null&&!uid.isEmpty())
+               uidR  = Restrictions.like("uid", "%"+uid+"%");
+            // To get records matching with AND condistions
+            if(nameR!=null&&uidR!=null)
+            {
+               LogicalExpression andExp = Restrictions.and(nameR, uidR);
+               cr.add( andExp );
+            }
+            else if(nameR==null&&uidR!=null)
+            {
+               System.out.println("adding uid R");
+               cr.add(uidR);
+            }
+            else if(nameR!=null&&uidR==null)
+            {
+               System.out.println("adding name R");
+               cr.add(nameR);
+            }
+            
+            custList = cr.list();
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            DaoDelegate.getInstance().close(session);
+        }
+        return custList;
+    }
+    
+}
---
+++ src/java/sg/gov/ida/kyc/data/CustomerDAOTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sg.gov.ida.kyc.data;
+
+import java.util.List;
+
+/**
+ *
+ * @author ftbs
+ */
+public class CustomerDAOTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*
+        CustomerDto dto = new CustomerDto();
+        dto.setUid("S9503806J");
+        dto.setName("Marvin");
+        dto.setAddress("Bukit Timah");
+        dto.setPhone("62111210");
+        CustomerDAO dao = new CustomerDAO();
+        dao.createCustomer(dto);
+        System.exit(0);
+                */
+        CustomerDAO dao = new CustomerDAO();
+        List<CustomerDto> list =  dao.search("MI", "");
+        for(int i=0;i<list.size();i++)
+        {
+            System.out.println("Name: "+list.get(i).getName());
+        }
+        
+        
+        System.exit(0);
+    }
+    
+}
