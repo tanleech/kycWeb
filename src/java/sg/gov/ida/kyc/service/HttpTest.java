ef59a41d11257e146473083785b62e94314aeb44
root <root@dcnfc>
2016-05-13 15:54:02 +0800

    json parser

--- src/java/sg/gov/ida/kyc/service/HttpTest.java
+++ src/java/sg/gov/ida/kyc/service/HttpTest.java
@@ -5,12 +5,12 @@
  */
 package sg.gov.ida.kyc.service;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
 import static javax.ws.rs.core.HttpHeaders.USER_AGENT;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -37,18 +37,6 @@ public class HttpTest {
 	// add header
 	post.setHeader("User-Agent", USER_AGENT);
         
-        /*
-	List urlParameters = new ArrayList();
-	urlParameters.add(new BasicNameValuePair("sn", "C02G8416DRJM"));
-	urlParameters.add(new BasicNameValuePair("cn", ""));
-	urlParameters.add(new BasicNameValuePair("locale", ""));
-	urlParameters.add(new BasicNameValuePair("caller", ""));
-	urlParameters.add(new BasicNameValuePair("num", "12345"));
-        
-	post.setEntity(new UrlEncodedFormEntity(urlParameters));
-
-        */
-        
 	HttpResponse response = client.execute(post);
 	System.out.println("Response Code : " 
                 + response.getStatusLine().getStatusCode());
@@ -56,35 +44,34 @@ public class HttpTest {
         BufferedReader rd = new BufferedReader(
 	        new InputStreamReader(response.getEntity().getContent()));
         
-        /*
-        JsonParser jsonParser = Json.createParser(rd);
+        //System.out.println("resp: "+rd.readLine());
+        JsonParser parser = new JsonParser();
+        Gson gson = new Gson();
+        String json=rd.readLine();
+                //"{\"Records\":\"{\"numEntries\":3,\"records\":[{\"recId\":\"S0000001ADBSUOB\",\"custId\":\"S0000001A\",\"custName\":\"Aladdin\",\"recOwner\":\"DBS\",\"encFor\":\"UOB\",\"encData\":\"LALALALALA\",\"dateCreated\":\"20160505\",\"dateModified\":\"20160505\"},{\"recId\":\"S0000001ADBSDBS\",\"custId\":\"S0000001A\",\"custName\":\"Aladdin\",\"recOwner\":\"DBS\",\"encFor\":\"DBS\",\"encData\":\"Xas8QPA7bB8ff7LwHfBMs82Cv8rJS5vPUwxHyNq77tbO9bc\",\"dateCreated\":\"20160501\",\"dateModified\":\"20160501\"},{\"recId\":\"S9516193FBAMLBAML\",\"custId\":\"S9516193F\",\"custName\":\"Marvin Chin\",\"recOwner\":\"BAML\",\"encFor\":\"BAML\",\"encData\":\"Marvin's info encryted for BAML\",\"dateCreated\":\"20160512\",\"dateModified\":\"20160512\"}]}\"}";
         
-        String keyName;
-        String value="";
-        while(jsonParser.hasNext())
-        {
-            Event event = jsonParser.next();
-            System.out.println("event: "+event);
-            switch (event) {
-            case KEY_NAME:
-                keyName = jsonParser.getString();
-                System.out.println("key: "+keyName);
-                break;
-            case VALUE_STRING:
-                value = jsonParser.getString();
-                System.out.println("value: "+value);
-                
-                break;
-            }
+        JsonElement element = parser.parse(json);
+        //JsonArray array = parser.parse(json).getAsJsonArray();
+        //String message = gson.fromJson(array.get(0), String.class);
+        //int number = gson.fromJson(array.get(1), int.class);
+        String rec =  element.getAsJsonObject().get("Records").getAsString();
+        JsonElement inner = parser.parse(rec);
+        JsonArray array = inner.getAsJsonObject().get("records").getAsJsonArray();
+        int num = inner.getAsJsonObject().get("numEntries").getAsInt();
+        //String message = gson.fromJson(array.get(2), String.class);
+        for(int i=0;i<array.size();i++){
+            System.out.println("recId:"  +array.get(i).getAsJsonObject().get("recId"));
+            System.out.println("custId:"  +array.get(i).getAsJsonObject().get("custId"));
+            System.out.println("custName:"  +array.get(i).getAsJsonObject().get("custName"));
+            System.out.println("recOwner:"  +array.get(i).getAsJsonObject().get("recOwner"));
+            System.out.println("encFor:"  +array.get(i).getAsJsonObject().get("encFor"));
+            System.out.println("encData:"  +array.get(i).getAsJsonObject().get("encData"));
+            
+            
         }
-*/
-        JsonReader jsonReader = Json.createReader(rd);
-        JsonObject jsonObject = jsonReader.readObject();
-        System.out.println("Field1: "+jsonObject.getString("Records"));
-        //System.out.println("Field2: "+jsonObject.getInt("numEntries"));
+        //System.out.println("Using Gson.fromJson() to get:"  +array.get(2).getAsJsonObject().get("custName"));
+
         
-        
-        jsonReader.close();
         rd.close();
         //JsonObject obj = jsonParser.;
         
